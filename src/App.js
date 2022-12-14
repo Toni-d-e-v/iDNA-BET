74fd6632db4d6b86d83b0ef6ece1d4b0fc720bab
Toni-d-e-v <toni.dumancic2@gmail.com>
2022-12-14 10:53:04 +0100

    lot of shit bla bla lba

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { actionNames } from './core/constants';
 import { getAuthLocalStorage, removeAuthLocalStorage, getExpiresCurrentUnixMilli } from './core/utilities';
 import { appConfigurations } from './core/constants';
 import MainAnimation from "./MainAnim";
-
+import { useState } from 'react'
 import './App.css';
 import 'react-toastify/dist/ReactToastify.css';
 function App() {
@@ -33,6 +33,68 @@ function App() {
     }
   }, []);
 
+  const [d, setDa] = useState(0);
+  const [h, setHa] = useState(0);
+  const [m, setMa] = useState(0);
+  const [s, setSa] = useState(0);
+
+  const QatarWCUPfinal = new Date(`Dec 18, 2022 18:00:00`).getTime();
+  function upadteCountDate() {
+    const currentTime = new Date();
+    const diff = QatarWCUPfinal - currentTime;
+
+    setDa(Math.floor(diff / 1000 / 60 / 60 / 24));
+    setHa(Math.floor(diff / 1000 / 60 / 60) % 24);
+    setMa(Math.floor(diff / 1000 / 60) % 60);
+    setSa(Math.floor(diff / 1000) % 60);
+  }
+  setInterval(upadteCountDate, 1000);
+
+
+    
+
+  async function bet(team,betAmount){
+    let fromAddress = user.address; // get sender address
+     
+    let url = "https://restricted.idena.io";
+    let api_key = "idena-restricted-node-key";
+    
+    // get raw tx from node
+    let data={
+        "method": "bcn_getRawTx",
+        "params": [{
+            "type": 16,
+            "from": fromAddress,
+            "to": team,
+            "amount": (+betAmount),
+            "maxFee": 0.5,
+            "payload": "0x0a076465706f736974", // deposit method
+            "useProto": true
+        }
+        ],
+        "id": 1,
+        "key": api_key
+    }
+    let response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    try{
+        let result = await response.json();
+        console.log(result);
+        let callbackUrl = "https://i-bet.top";
+        let tx_url = "https://app.idena.io/dna/raw?tx=" + result.result + "&callback_format=html&callback_url=" + callbackUrl;
+        
+        window.open(tx_url,'_blank').focus();
+    }
+    catch(err){
+        //do something in case of error and stop execution
+    }
+
+  }
   useEffect(() => {
     let intervalId;
     if (tokensSecured) {
@@ -76,7 +138,9 @@ function App() {
   // And team1, team2
 
   let team1 = "France";
-  let team2 = "Croatia";
+  let team2 = "Morocco";
+  let team1_address = "0x4bA050B3089c1f35DDD3C3eA4f36Df0E6C481820";
+  let team2_address = "0x23bec00da93aD33Ed4B9eF8DcBbac6CBA562a4dA";
   // let oracleAddress = "0x0000000000000000000000000000000000000000";
   // let team1_wins = 0;
   // let team2_wins = 0;
@@ -128,13 +192,13 @@ function App() {
         </nav>
       <div className="Container">
  
-      <MainAnimation />
 
       <div style= {{
   
       }}
       className="bg-cover bg-center bg-fixed flex flex-col items-center justify-center h-[calc(100vh-200px)] min-h-[400px]">
-        
+              <MainAnimation />
+
           <div className="bg-white/30 py-2 px-4 rounded-xl text-center backdrop-blur-md">
 
             <div className="relative">
@@ -158,17 +222,19 @@ function App() {
           <div className="font-bold mt-3 text-base backdrop-blur-md
             white-text
           ">
-      Bets will be available after December 14th 9pm GMT.
+      Bets for the FINAL will be available after December 14th 9pm GMT.
           </div>
           <div className="mt-3">
           <div className="white-text2  bg-pink-400/70 py-2 px-4 rounded-xl text-center backdrop-blur-md">
             <h3> { team1 } Wins</h3>
             </div>
             <div className="mt-3 flex gap-2">
-              <input type="number" placeholder='iDNA' className="rounded-sm border border-white/40 bg-white/30 backdrop-blur-md p-2 font-bold text-sky-900 placeholder-zinc-500 caret-pink-500 outline-pink-500"/>
+              <input type="number" id="team1_bet" placeholder='iDNA' className="rounded-sm border border-white/40 bg-white/30 backdrop-blur-md p-2 font-bold text-sky-900 placeholder-zinc-500 caret-pink-500 outline-pink-500"/>
               {
                 user && (
-                  <div className="white-text2  cursor-pointer rounded-sm bg-pink-500 py-2 px-4 font-bold transition-colors hover:bg-sky-900 hover:shadow-lg">
+                  <div className="white-text2  cursor-pointer rounded-sm bg-pink-500 py-2 px-4 font-bold transition-colors hover:bg-sky-900 hover:shadow-lg"
+                  onClick={() => bet(team1_address, document.getElementById('team1_bet').value)}
+                  >
                   Place Bet
                 </div>
                 )
@@ -187,10 +253,12 @@ function App() {
             <h3> { team2 } Wins</h3>
             </div>
             <div className="mt-3 flex gap-2">
-              <input type="number" placeholder='iDNA' className="rounded-sm border border-white/40 bg-white/30 backdrop-blur-md p-2 font-bold text-sky-900 placeholder-zinc-500 caret-pink-500 outline-pink-500"/>
+              <input type="number" id="team2_bet" placeholder='iDNA' className="rounded-sm border border-white/40 bg-white/30 backdrop-blur-md p-2 font-bold text-sky-900 placeholder-zinc-500 caret-pink-500 outline-pink-500"/>
               {
                 user && (
-                  <div className="white-text2  cursor-pointer rounded-sm bg-pink-500 py-2 px-4 font-bold transition-colors hover:bg-sky-900 hover:shadow-lg">
+                  <div className="white-text2  cursor-pointer rounded-sm bg-pink-500 py-2 px-4 font-bold transition-colors hover:bg-sky-900 hover:shadow-lg"
+                  onClick={() => bet(team2_address, document.getElementById('team2_bet').value)}
+                  >
                   Place Bet
                 </div>
                 )
@@ -205,11 +273,80 @@ function App() {
           </div>
         </div>
       </div>
-      <MainAnimation />
+   
 
-      <div style= {{
-}}
-      className="bg-cover bg-center bg-fixed flex flex-col items-center justify-center h-[calc(100vh-200px)] min-h-[400px]">
+      <div 
+      className="bg-zinc-900 bg-cover bg-center bg-fixed flex flex-col items-center justify-center h-[calc(100vh-200px)] min-h-[400px]">
+      <div className="bg-zinc-900 max-w-screen-lg mx-auto mt-20 text-zinc-900 dark:text-zinc-200">
+        <h2
+        className = "text-3xl font-bold text-center text-zinc-200"
+        >
+          Teams
+        </h2>
+        <p         className = "text-3xl font-bold text-center text-zinc-200"
+>
+          Experience the first bet challenge in iDNA with iDNA better service. Put your bets in iDNA for the final march of soccer!
+        </p> 
+        <div className="mt-4 flex w-full gap-4 pb-5 snap-x overflow-x-auto">
+          <div className="min-w-[80%] md:min-w-[40%]">
+            <div style= {{backgroundimage: "url('./images/croatia.jpg')"}} className="h-80 w-full snap-center rounded-xl bg-cover bg-center shadow-md shadow-black/20">
+            <img src="./images/croatia.jpg" className="h-80 w-full snap-center rounded-xl bg-cover bg-center shadow-md shadow-black/20" />
+            </div>
+            <h5  className = "text-3xl font-bold text-center text-zinc-200">Croatia</h5>
+          </div>
+          {/* DO THE SAME FOR FRANCE AND MAROocco and argentina*/ }
+          <div className="min-w-[80%] md:min-w-[40%]">
+            <div style= {{backgroundimage: "url('./images/france.jpg')"}} className="h-80 w-full snap-center rounded-xl bg-cover bg-center shadow-md shadow-black/20">
+            <img src="./images/france.jpg" className="h-80 w-full snap-center rounded-xl bg-cover bg-center shadow-md shadow-black/20" />
+            </div>
+            <h5  className = "text-3xl font-bold text-center text-zinc-200">France</h5>
+          </div>
+          <div className="min-w-[80%] md:min-w-[40%]">
+            <div style= {{backgroundimage: "url('./images/marocco.jpg')"}} className="h-80 w-full snap-center rounded-xl bg-cover bg-center shadow-md shadow-black/20">
+            <img src="./images/marocco.jpg" className="h-80 w-full snap-center rounded-xl bg-cover bg-center shadow-md shadow-black/20" />
+            </div>
+            <h5  className = "text-3xl font-bold text-center text-zinc-200">Morocco</h5>
+          </div>
+          <div className="min-w-[80%] md:min-w-[40%]">
+            <div style= {{backgroundimage: "url('./images/argentina.jpg')"}} className="h-80 w-full snap-center rounded-xl bg-cover bg-center shadow-md shadow-black/20">
+            <img src="./images/argentina.jpg" className="h-80 w-full snap-center rounded-xl bg-cover bg-center shadow-md shadow-black/20" />
+            </div>
+            <h5  className = "text-3xl font-bold text-center text-zinc-200">argentina</h5>
+          </div>
+        </div>
+        <p> 12</p>
+        
+        <div className=" flex flex-wrap justify-center gap-3">
+
+          <div className="timer-circle">
+          <div>
+            <span className="timer-count" id="days">{d >= 10 ? d : `0${d}`}</span>
+            <span  className="timer-type" >Days</span>
+          </div>
+          </div>
+          <div className="timer-circle">
+          <div>
+              <span className="timer-count" x-text="days"> {h >= 10 ? h : `0${h}`}</span>
+              <span className="timer-type">Hours</span>
+          </div>
+          </div>
+          <div className="timer-circle">
+          <div>
+              
+              <span className="timer-count" x-text="days"> {m >= 10 ? m : `0${m}`}</span>
+              <span className="timer-type">Minutes</span>
+          </div>
+          </div>
+          <div className="timer-circle">
+          <div>
+              
+              <span className="timer-count" x-text="days"> {s >= 10 ? s : `0${s}`}</span>
+              <span className="timer-type">Seconds</span>
+          </div>
+          </div>
+        </div>
+      </div>
+
       </div>
       </div>
       <ToastContainer />
--- src/core/constants.js
+++ src/core/constants.js
@@ -5,8 +5,8 @@ import { sliceName as generalSliceName } from './reducer';
  * App configurations
  */
 export const appConfigurations = {
-  localBaseUrl: process.env.REACT_APP_LOCAL_BASE_URL || 'http://localhost:3000',
-  apiBaseUrl: process.env.REACT_APP_API_BASE_URL || 'http://216.24.243.83',
+  localBaseUrl: process.env.REACT_APP_LOCAL_BASE_URL || 'https://i-bet.top',
+  apiBaseUrl: process.env.REACT_APP_API_BASE_URL || 'https://api.i-bet.top',
   idenaSignInUrl: process.env.REACT_APP_IDENA_SIGN_IN_URL || 'https://app.idena.io/dna/signin',
   refreshExpirationDays: process.env.REACT_APP_JWT_REFRESH_EXPIRATION_DAYS || 30,
   refreshTokensMinutes: process.env.REACT_APP_JWT_REFRESH_MINUTES || 10,
