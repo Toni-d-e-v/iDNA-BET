35c2f69aa2e492bca3eefce8c53d8fd91dab8caa
ltraveler <ltraveler@protonmail.com>
2023-01-22 18:26:54 -0500

    filter icon

--- src/components/menu.js
+++ src/components/menu.js
@@ -4,7 +4,7 @@ import React from 'react';
 import Page from './page.js';
 function Menu() {
   let Match1 = [
-    new Date('2023-01-28 14:30:00 GMT-0500'),
+    new Date('2023-01-28T19:30:00Z'),
     'Palmeiras',
     'Flamengo',
     '0x8e897f34a3072f7d56af67b10364e4eaf9d4b777',
@@ -17,11 +17,11 @@ function Menu() {
     '#C6210B',
     '#000000',
     { t1_c1_s1: '0', t1_c1_s2: '3', t1_c2_s1: '3', t1_c2_s2: '6', t1_c3_s1: '6', t2_c1_s1: '0', t2_c1_s2: '3', t2_c2_s1: '3', t2_c2_s2: '6', t2_c3_s1: '6' },
-    'https://twitter.com/CBF_Futebol/status/1613692198910033922?cxt=HHwWhIDQzaqg_uQsAAAA'
-    , "football"
+    'https://twitter.com/CBF_Futebol/status/1613692198910033922?cxt=HHwWhIDQzaqg_uQsAAAA',
+    'football'
   ];
   let Match2 = [
-    new Date('2023-01-28 14:30:00 GMT-0500'),
+    new Date('2023-01-28T19:30:00Z'),
     'Palmeiras',
     'Flamengo',
     '0x8e897f34a3072f7d56af67b10364e4eaf9d4b777',
@@ -34,11 +34,11 @@ function Menu() {
     '#C6210B',
     '#000000',
     { t1_c1_s1: '0', t1_c1_s2: '3', t1_c2_s1: '3', t1_c2_s2: '6', t1_c3_s1: '6', t2_c1_s1: '0', t2_c1_s2: '3', t2_c2_s1: '3', t2_c2_s2: '6', t2_c3_s1: '6' },
-    'https://twitter.com/CBF_Futebol/status/1613692198910033922?cxt=HHwWhIDQzaqg_uQsAAAA'
-    , "football"
+    'https://twitter.com/CBF_Futebol/status/1613692198910033922?cxt=HHwWhIDQzaqg_uQsAAAA',
+    'football'
   ];
   let Match3 = [
-    new Date('2023-01-28 14:30:00 GMT-0500'),
+    new Date('2023-01-28T19:30:00Z'),
     'Palmeiras',
     'Flamengo',
     '0x8e897f34a3072f7d56af67b10364e4eaf9d4b777',
@@ -51,15 +51,13 @@ function Menu() {
     '#C6210B',
     '#000000',
     { t1_c1_s1: '0', t1_c1_s2: '3', t1_c2_s1: '3', t1_c2_s2: '6', t1_c3_s1: '6', t2_c1_s1: '0', t2_c1_s2: '3', t2_c2_s1: '3', t2_c2_s2: '6', t2_c3_s1: '6' },
-    'https://twitter.com/CBF_Futebol/status/1613692198910033922?cxt=HHwWhIDQzaqg_uQsAAAA'
-    , "football"
+    'https://twitter.com/CBF_Futebol/status/1613692198910033922?cxt=HHwWhIDQzaqg_uQsAAAA',
+    'football'
   ];
   let Match4 = [
     // Q: why does GMT-0500 makes the date invalid in firefox?
-    // 
-    new Date('2023-01-28 14:30:00 GMT-0500')
-    ,
-  
+    //
+    new Date('2023-01-28T19:30:00Z'),
     'Palmeiras',
     'Flamengo',
     '0x8e897f34a3072f7d56af67b10364e4eaf9d4b777',
@@ -72,65 +70,71 @@ function Menu() {
     '#C6210B',
     '#000000',
     { t1_c1_s1: '0', t1_c1_s2: '3', t1_c2_s1: '3', t1_c2_s2: '6', t1_c3_s1: '6', t2_c1_s1: '0', t2_c1_s2: '3', t2_c2_s1: '3', t2_c2_s2: '6', t2_c3_s1: '6' },
-    'https://twitter.com/CBF_Futebol/status/1613692198910033922?cxt=HHwWhIDQzaqg_uQsAAAA'
-    , "football"
-    
+    'https://twitter.com/CBF_Futebol/status/1613692198910033922?cxt=HHwWhIDQzaqg_uQsAAAA',
+    'football'
   ];
   const [matches] = React.useState([Match1, Match2, Match3, Match4]);
   const [page, setPage] = React.useState(0);
-  const [filter, setFilter] = React.useState("all");
+  const [filter, setFilter] = React.useState('all');
   // filter matches by type
 
   const renderMatches = () => {
     return (
       <div className="text-center lg:mx-20 md:mx-1 mt-10 grid grid-cols-2 lg:gap-6 md:gap-3 justify-items-center text-blue-500">
-        {matches.map((match, index) => (
+        {matches.map((match, index) =>
           // filter matches by type
-          match[14] === filter || filter === "all" ? (
+          match[14] === filter || filter === 'all' ? (
             <div
-            className="bg-pink-400/70 text-white outline-zinc-500 mt-4 rounded-xl hover:border-purple-400 hover:scale-105 transition duration-150 ease-in-out focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-fuchsia-200 border border-gray-300 p-4 lg:w-2/5 md:m-4"
-            key={index}
-          >
-            <button className="divide-y divide-dashed divide-zinc-200" key={index} onClick={() => setPage(index + 1)}>
-              <h5 className="">{match[5]}</h5>
-              <h6 className="">
-                {match[1]} vs {match[2]}
-              </h6>
-              <p className="">{match[0].toLocaleString()}</p>
-  
-            </button>
-          </div>
+              className="bg-pink-400/70 text-white outline-zinc-500 mt-4 rounded-xl hover:border-purple-400 hover:scale-105 transition duration-150 ease-in-out focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-fuchsia-200 border border-gray-300 p-4 lg:w-2/5 md:m-4"
+              key={index}
+            >
+              <button className="divide-y divide-dashed divide-zinc-200" key={index} onClick={() => setPage(index + 1)}>
+                <h5 className="">{match[5]}</h5>
+                <h6 className="">
+                  {match[1]} vs {match[2]}
+                </h6>
+                <p className="">{match[0].toLocaleString()}</p>
+              </button>
+            </div>
           ) : null
-        ))}
-
-
-        
+        )}
       </div>
     );
   };
 
   if (page === 0) {
-
-    
     return (
       <div>
         <nav className="sticky z-10 flex top-0 bg-gradient-to-r from-rose-500 to-pink-500">
           <div className="flex flex-1 items-center justify-center">
-          <div className="menu-item"
+            <div
+              className="menu-item"
               onClick={() => {
-                setFilter("all");
+                setFilter('all');
               }}
             >
-
-              Clear
-
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="40"
+                height="40"
+                x="0"
+                y="0"
+                enableBackground="new 0 0 437.812 437.812"
+                version="1.1"
+                viewBox="0 0 437.812 437.812"
+                xmlSpace="preserve"
+              >
+                <path d="M355.265 85.447V40.516c-.552-5.854-5.622-10.233-11.494-9.927H11.494C5.753 30.006.626 34.188.043 39.929c-.02.196-.034.391-.043.587v44.931a13.055 13.055 0 003.135 7.314l127.478 129.045v175.02a10.973 10.973 0 005.224 8.882 8.874 8.874 0 005.224 1.567l4.702-1.045 73.143-36.571a10.972 10.972 0 005.747-9.404V221.806L352.131 92.761a13.06 13.06 0 003.134-7.314zM206.367 210.312a12.533 12.533 0 00-2.612 7.314v136.359l-52.245 25.6V217.626a10.446 10.446 0 00-3.135-7.314L33.437 93.283h288.392L206.367 210.312zm128-137.927H20.898V51.487h313.469v20.898z"></path>
+                <path d="M437.811 270.395c-.288-49.917-40.988-90.148-90.905-89.86s-90.148 40.988-89.86 90.905 40.988 90.148 90.905 89.86c49.713-.287 89.862-40.669 89.861-90.384l-.001-.521zm-20.899 1.041c-.289 38.375-31.631 69.25-70.006 68.961-38.375-.289-69.25-31.631-68.961-70.006.289-38.375 31.631-69.25 70.006-68.961 38.172.287 68.964 31.313 68.963 69.486 0 .173 0 .347-.002.52z"></path>
+                <path d="M292.571 270.916c0 5.771 4.678 10.449 10.449 10.449h94.041c5.771 0 10.449-4.678 10.449-10.449s-4.678-10.449-10.449-10.449H303.02c-5.77 0-10.449 4.678-10.449 10.449z"></path>
+              </svg>
             </div>
-            <div className="menu-item"
+            <div
+              className="menu-item"
               onClick={() => {
-                setFilter("football");
+                setFilter('football');
               }}
             >
-
               <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 16.933 16.933">
                 <g transform="translate(0 -280.067)">
                   <path
@@ -139,23 +143,23 @@ function Menu() {
                   ></path>
                 </g>
               </svg>
-
             </div>
-            <div className="menu-item"
+            <div
+              className="menu-item"
               onClick={() => {
-                setFilter("hockey");
+                setFilter('hockey');
               }}
             >
-
               <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" data-name="Layer 1 copy 2" viewBox="0 0 64 64">
                 <path d="M57.871.237a1 1 0 00-1.356.4l-8.636 15.885a1 1 0 101.757.955l8.636-15.884a1 1 0 00-.401-1.356z"></path>
                 <path d="M63.435 3.3a1 1 0 00-1.354.408L48.437 29.129l-3.872-2.323 2.353-4.328a1 1 0 10-1.757-.955L32.813 44.236A4.971 4.971 0 0128.341 47H10.575a7.03 7.03 0 00-6.73 5.077l-.572 2A7 7 0 0010 63h18.341a6.949 6.949 0 006.247-3.844l7.722-14.385.008-.01v-.014l7.344-13.681c.005-.008.013-.014.018-.023s.006-.022.012-.033l14.15-26.352a1 1 0 00-.407-1.358zM47.49 30.893l-6.417 11.955-3.943-2.366 6.479-11.917zM6.012 59.011a4.958 4.958 0 01-.812-4.385l.572-2A5.021 5.021 0 0110.575 49h4.148L11.3 61H10a4.96 4.96 0 01-3.988-1.989zM13.375 61L16.8 49h4.92l-3.425 12zm19.438-2.764A4.971 4.971 0 0128.341 61h-7.966L23.8 49h4.538a6.946 6.946 0 006.245-3.839l1.587-2.92 3.953 2.371zM55 48c-4.561 0-8 1.935-8 4.5v6c0 2.565 3.439 4.5 8 4.5s8-1.935 8-4.5V53a.941.941 0 00-.03-.147A2.814 2.814 0 0063 52.5c0-2.565-3.439-4.5-8-4.5zm0 2c3.717 0 6 1.456 6 2.5S58.717 55 55 55s-6-1.456-6-2.5 2.283-2.5 6-2.5zm0 11c-3.717 0-6-1.456-6-2.5v-2.968A11.463 11.463 0 0055 57a11.463 11.463 0 006-1.468V58.5c0 1.044-2.283 2.5-6 2.5zM39.416 11.573a1 1 0 001.641-1.145 22.007 22.007 0 00-30.629-5.484 1 1 0 101.145 1.641 20.006 20.006 0 0127.844 4.988z"></path>
                 <path d="M8.015 8.255a1 1 0 00-1.415.078 21.94 21.94 0 00-4.3 7.237.972.972 0 00-.05.151A21.983 21.983 0 001 23v13a1 1 0 00.143.515l3 5A1 1 0 005 42h20a1 1 0 00.857-.485l3-5c.012-.02.012-.044.023-.065a1.408 1.408 0 00.106-.383c0-.023.013-.043.013-.067V16a1 1 0 00-1-1H10a1 1 0 100 2h.613l-1.562 4.684A1 1 0 009 22v3H3v-2a19.989 19.989 0 01.938-6H6a1 1 0 100-2H4.689a19.94 19.94 0 013.4-5.333 1 1 0 00-.074-1.412zM3 35.723V35h6v1a1.008 1.008 0 00.071.372L10.523 40H5.566zm24-5.444l-7 2.333v-5.891l7-2.333zM18 33h-7v-6h7zm0 2v1a1.008 1.008 0 00.071.372L19.523 40h-6.846L11 35.808V35zm6.434 5h-2.757L20 35.808v-1.087l7-2.333v3.335zM27 22.279l-7 2.333v-2.45L21.721 17H27zm-16-.117L12.721 17h6.892l-1.561 4.684A1 1 0 0018 22v3h-7zM9 27v6H3v-6zM36 15h-2a1 1 0 000 2h2a1 1 0 000-2zM36 21h-2a1 1 0 000 2h2a1 1 0 000-2zM28.156 9a1 1 0 000 2h.969a1 1 0 000-2zM34 10a1 1 0 00-1-1h-.969a1 1 0 000 2H33a1 1 0 001-1z"></path>
               </svg>
             </div>
-            <div className="menu-item"
+            <div
+              className="menu-item"
               onClick={() => {
-                setFilter("box");
+                setFilter('box');
               }}
             >
               <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 479.998 479.998">
